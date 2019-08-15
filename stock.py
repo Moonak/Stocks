fe6f8c66bb24db866ad5fe909558521f1c555260
Mohammad Afkani <37617376+Moonak@users.noreply.github.com>
2019-08-15 15:52:28 +0430

    update func load

--- stock.py
+++ stock.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import MinMaxScaler
 class load:
     def __init__ (self,filename):
         self.filename = filename
-        self.data   = np.genfromtxt('StockData/'+self.filename,delimiter=',',dtype=(int,float,float,float,float,float,float,str))
+        self.data   = np.genfromtxt('StockData/'+self.filename,delimiter=',')
         self.date   = \
         np.array( [date(int(str(i)[:4]),int(str(i)[4:6]),int(str(i)[6:8]))\
                  for i in self.data[1:,0]] )
